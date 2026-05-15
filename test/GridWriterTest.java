93050b8b30ef2f63a72fad45133556cdf88cf548
yeetu <hussainrehan909@gmail.com>
2026-05-15 04:32:12 -0400

    GridWriterTests

--- test/GridWriterTest.java
+++ test/GridWriterTest.java
@@ -5,6 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class GridWriterTest {
 
     @Test
-    void testMazeWorks() {
+     void testSpaceTypesAreCorrect() {
+
+        Space start = new Space("S", 0, 0, 2);
+        Space end = new Space("E", 0, 1, 2);
+        Space wall = new Space("X", 1, 0, 2);
+        Space open = new Space("O", 1, 1, 2);
+
+        assertTrue(start.isStart());
+        assertTrue(end.isEnd());
+        assertTrue(wall.isBlocked());
+        assertFalse(open.isBlocked());
+    }
+    @Test
+     void testPathMarking() {
+
+        Space s = new Space("O", 0, 0, 2);
+
+        assertFalse(s.isOnPath());
+
+        s.setOnPath(true);
+
+        assertTrue(s.isOnPath());
     }
 }
